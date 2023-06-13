6ddf179884b0f0998647acd08b900dc9d5aee95a
VictorMRD <victormrd2002@hotmail.com>
2023-06-13 09:27:33 -0700

    e

--- resources/views/livewire/paravotar.blade.php
+++ resources/views/livewire/paravotar.blade.php
@@ -1,14 +1,13 @@
 <div class="w-screen h-screen overflow-auto box-border">
     {{-- Success is as dangerous as failure. --}}
-    <div class="flex flex-col gap-10 bg-black justify-center items-center h-screen w-screen overflow-auto">
+    <div class="flex flex-col gap-10 bg-black justify-center items-center h-screen w-screen border-2">
         <p class="text-white font-semibold text-6xl">Votaciones</p>
         <p class="text-white font-semibold text-4xl"><a href="/index">Regresar</a></p>
-        <div class="flex flex-col gap-5 pt-10 bg-black justify-center items-center h-96 w-screen overflow-auto">
+        <div class="flex flex-col gap-2 bg-black items-center h-96 w-full overflow-auto">
             @foreach ($votaciones as $encuesta)
-                <div class="w-2/4 flex flex-col justify-center items-center bg-black text-white gap-6
-                    shadow-md shadow-white select-none p-8">
+                <div class="w-2/4 h-72 flex flex-col bg-black text-white shadow-md shadow-white select-none">
                     <p class="text-4xl text-center font-semibold">{{ $encuesta->pregunta }}</p>
-                    <div class="flex text-center justify-center items-center gap-6">
+                    <div class="flex text-center justify-center items-center gap-6 h-40">
                         <div class="hover:scale-110">
                             <button onclick="votar({{ $encuesta->id }},1)" class="text-xl font-semibold">{{ $encuesta->respuesta1 }}</button>
                         </div>
